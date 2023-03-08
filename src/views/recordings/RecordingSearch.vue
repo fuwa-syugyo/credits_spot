d89e891c5e9c1dfadb570a43ef8ca83f24e97593
fuwa <fuwafuwa41.glr@gmail.com>
2023-03-08 17:12:18 +0900

    offset=1のときのみ表示できるようにした

--- src/views/recordings/RecordingSearch.vue
+++ src/views/recordings/RecordingSearch.vue
@@ -1,25 +1,60 @@
 <script setup lang="ts">
   import { ref, onMounted } from "vue";
 
+  type RecordingData = {
+      id: string;
+      title: string;
+      artist: string;
+      first_release_date: string;
+    };
+
+    const recording_data = ref<RecordingData[]>([]);
+
     const onClickHandler = async (page: number) => {
       console.log(page);
       const term = "ありがとう"
-      const res = await fetch(`https://musicbrainz.org/ws/2/recording/?query=recording:${term}&offset=&${page}&fmt=json`)
+      const res = await fetch(`https://musicbrainz.org/ws/2/recording/?query=recording:${term}&offset=${page}&limit=100&fmt=json`)
       const data = await res.json();
-      console.log(data);
-    };
+
+    const new_recording_data: RecordingData[] = data.recordings.filter((rec:RecordingData) => rec).map((item: RecordingData) => ({
+      id: item.id,
+      title: item.title,
+      artist: item["artist-credit"][0].name,
+      first_release_date: item["first-release-date"]
+    }))
+
+    recording_data.value = new_recording_data;
+
+    console.log(recording_data.value)
+  }
 
     const currentPage = ref(1);
 
-    onMounted(() => {
-      onClickHandler(currentPage.value)
+    onMounted(async () => {
+      await onClickHandler(currentPage.value)
     })
 </script>
 
 <template>
+  <table>
+    <thead>
+      <tr>
+        <th>曲名</th>
+        <th>アーティスト</th>
+        <th>リリース日</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="recording in recording_data" :key="recording.id">
+        <td>{{ recording.title }}</td>
+        <td>{{ recording.artist }}</td>
+        <td>{{ recording.first_release_date }}</td>
+      </tr>
+    </tbody>
+  </table>
   <vue-awesome-paginate
-    :total-items="50"
-    :items-per-page="5"
+    :total-items="1000"
+    :items-per-page="100"
     :max-pages-shown="5"
     v-model="currentPage"
     :on-click="onClickHandler"
