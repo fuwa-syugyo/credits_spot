98ba7a3a6a8a9e72a66ac2b2850e906854513b96
fuwa <fuwafuwa41.glr@gmail.com>
2023-03-30 11:28:24 +0900

    ページネーションボタンの数字を中央揃えにした

--- src/components/recordings/RecordingSearch.vue
+++ src/components/recordings/RecordingSearch.vue
@@ -98,26 +98,30 @@
   />
 </template>
 
-<style scoped>
+<style>
   .pagination-container {
     display: flex;
     column-gap: 10px;
   }
+
   .paginate-buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 40px;
-    width: 40px;
-    border-radius: 20px;
+    width: 60px;
+    border-radius: 10px;
     cursor: pointer;
     background-color: rgb(242, 242, 242);
     border: 1px solid rgb(217, 217, 217);
     color: black;
   }
+
   .paginate-buttons:hover {
     background-color: #d8d8d8;
   }
   .active-page {
     background-color: #3498db;
-    border: 1px solid #3498db;
     color: white;
   }
   .active-page:hover {
