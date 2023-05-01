7e3cb172533565a127edbd98f2c494df95f45f40
fuwa <fuwafuwa41.glr@gmail.com>
2023-05-01 23:02:10 +0900

    楽曲検索結果に想定通りの内容が含まれているかのテストを追加

--- cypress/e2e/flow_recording_search.spec.cy.ts
+++ cypress/e2e/flow_recording_search.spec.cy.ts
@@ -3,8 +3,10 @@ describe('Recording Search', () => {
     cy.visit('http://127.0.0.1:5173/')
 
     cy.get('input[type="search"]').should('be.visible').type('ミックスナッツ', {force: true})
-
     cy.get('button[type="submit"]').click()
+
     cy.url().should('include', '/recordings')
+    cy.contains('ミックスナッツ')
+    cy.contains('Official髭男dism')
   })
 })
