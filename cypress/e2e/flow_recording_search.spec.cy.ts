4141e5c56e8e60ce55b0399e8fa56cc7124971ad
fuwa <fuwafuwa41.glr@gmail.com>
2023-05-01 22:35:10 +0900

    テストで検索フォームに楽曲名を入力して検索結果を表示するようにした

---
+++ cypress/e2e/flow_recording_search.spec.cy.ts
@@ -0,0 +1,10 @@
+describe('Recording Search', () => {
+  it('Visits recording search result', () => {
+    cy.visit('http://127.0.0.1:5173/')
+
+    cy.get('input[type="search"]').should('be.visible').type('ミックスナッツ', {force: true})
+
+    cy.get('button[type="submit"]').click()
+    cy.url().should('include', '/recordings')
+  })
+})
