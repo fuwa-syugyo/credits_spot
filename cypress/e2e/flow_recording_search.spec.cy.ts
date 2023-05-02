ad27b9c3d6d258e3946d1b9b7ab33b7d542c7b4b
fuwa <fuwafuwa41.glr@gmail.com>
2023-05-02 09:43:36 +0900

    楽曲詳細画面の表示のテストを追加

--- cypress/e2e/flow_recording_search.spec.cy.ts
+++ cypress/e2e/flow_recording_search.spec.cy.ts
@@ -1,4 +1,4 @@
-describe('Recording Search', () => {
+describe('Recording search and lookup artist', () => {
   it('Visits recording search result', () => {
     cy.visit('http://127.0.0.1:5173/')
 
@@ -8,5 +8,14 @@ describe('Recording Search', () => {
     cy.url().should('include', '/recordings')
     cy.contains('ミックスナッツ')
     cy.contains('Official髭男dism')
+
+    cy.contains('ミックスナッツ').click()
+    cy.url().should('include', '/recordings/30ed18b6-d95b-4efe-a332-6fcc0e07b087')
+    cy.contains('ミックスナッツ')
+    cy.contains('Official髭男dism')
+    cy.contains('composer')
+    cy.contains('藤原聡')
+    cy.contains('trombone')
+    cy.contains('川島稔弘')
   })
 })
