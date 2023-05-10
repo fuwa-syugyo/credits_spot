42bab59ef3d739aa656b640c0f4a82c31bc091a4
fuwa <fuwafuwa41.glr@gmail.com>
2023-05-10 21:50:21 +0900

    100件以下の検索結果のテストを追加

--- cypress/component/RecordingSearch.cy.ts
+++ cypress/component/RecordingSearch.cy.ts
@@ -2,7 +2,11 @@ import RecordingSearch from '../../src/components/recordings/RecordingSearch.vue
 
 it('Less than 100 recording search result', () => {
   cy.mount(RecordingSearch, { query: { term: 'ミックスナッツ' } })
-  cy.contains('ミックスナッツ')
-
+  cy.get('.table-auto > tbody > :nth-child(1) >  > :nth-child(1)')
+    .contains('ミックスナッツ')
+  cy.get('.table-auto > tbody > :nth-child(1) > :nth-child(2)')
+    .contains('Official髭男dism')
+  cy.get('.table-auto > tbody > :nth-child(1) > :nth-child(3)')
+    .contains('2022-04-15')
 })
 
