3d8e87f3000c3cd7ee6b3b7442095549569bbaa8
fuwa <fuwafuwa41.glr@gmail.com>
2023-05-25 14:19:42 +0900

    不要なテスト削除＆インデント修正

--- cypress/e2e/flow_artist_search.spec.cy.ts
+++ cypress/e2e/flow_artist_search.spec.cy.ts
@@ -1,14 +1,11 @@
 describe('template spec', () => {
   it('Visits artist search result', function() {
     cy.visit('http://127.0.0.1:5173/')
-    // cy.mount(SearchForm)
     cy.intercept('GET', 'https://musicbrainz.org/ws/2/artist/?query=artist:%E5%B0%8F%E5%AE%A4%E5%93%B2%E5%93%89&offset=0&limit=100&fmt=json', { fixture: 'mock_komurotetsuya_page1.json' }).as('komurotetsuya1PageRequest');
 
     cy.get('[value="人物名"]').check();
     cy.get('#search').type('小室哲哉{enter}', {force: true});
     cy.get('.text-white').click();
-
-    // cy.mount(ArtistSearch, { query: { term: '小室哲哉' } })
     cy.wait('@komurotetsuya1PageRequest');
     cy.contains('小室哲哉')
 
@@ -16,7 +13,6 @@ describe('template spec', () => {
     cy.intercept('GET', 'https://musicbrainz.org/ws/2/artist/?query=artist:%E5%B0%8F%E5%AE%A4%E5%93%B2%E5%93%89&offset=100&limit=100&fmt=json', { fixture: 'mock_komurotetsuya_page2.json' }).as('komurotetsuya2PageRequest');
     cy.get(':nth-child(7) > .paginate-buttons').click();
     cy.wait('@komurotetsuya2PageRequest');
-
     cy.get('body').should('not.contain', 'Undefined')
     cy.get('.back-button').click();
 
@@ -34,12 +30,9 @@ describe('template spec', () => {
     cy.get('.artist-table > tbody > :nth-child(1) > td > a ').click();
     cy.wait('@komurotetsuyaDWakareRelationshipRequest');
     cy.wait('@komurotetsuyaDWakareSpotifyRequest');
-    cy.url().should('include', '/recordings/08cea5ad-09af-466a-b2f4-46ec63dd2d09')
-
     cy.get('.text-2xl').contains('Dのテーマ (別れ)')
     cy.get('.my-2 > tbody > tr > .px-4').contains('小室哲哉')
-    cy.get(':nth-child(1) > :nth-child(6) > :nth-child(2) > a')
-    .should('not.be')
+    cy.get(':nth-child(1) > :nth-child(6) > :nth-child(2) > a').should('not.be')
     cy.go('back')
 
     //workへ
@@ -56,8 +49,8 @@ describe('template spec', () => {
 
     cy.get('.text-2xl').contains('CRAZY GONNA CRAZY')
     cy.get('table tbody tr')
-    .contains('composer').parent().parent()
-    .contains('小室哲哉')
+      .contains('composer').parent().parent()
+      .contains('小室哲哉')
     cy.get(':nth-child(6) > :nth-child(2) > a').contains('Spotifyで聴く');
     cy.go('back')
     cy.go('back')
@@ -65,17 +58,15 @@ describe('template spec', () => {
     //スタッフ曲へ
     cy.intercept('GET', 'https://musicbrainz.org/ws/2/recording/ff0a6f14-14b2-4b32-b84b-b6961293b92c?inc=artist-credits+recording-rels+work-rels+work-level-rels+artist-rels+isrcs&fmt=json', { fixture: 'mock_soon_19_relationship.json' }).as('komurotetsuyaSoon19RelationshipRequest');
     cy.intercept('GET', 'https://api.spotify.com/v1/search?query=isrc%3Aundefined&type=track&offset=0&limit=20', { fixture: 'mock_soon_19_spotify.json' }).as('komurotetsuyaSoon19SpotifyRequest');
-
     cy.get('.staff-table > tbody > :nth-child(1) > :nth-child(2) > a').click();
     cy.wait('@komurotetsuyaSoon19RelationshipRequest');
     cy.wait('@komurotetsuyaSoon19SpotifyRequest');
-
     cy.get('.text-2xl').contains('...soon nineteen')
     cy.get('table tbody tr')
-    .contains('arranger').parent().parent()
-    .contains('小室哲哉')
+      .contains('arranger').parent().parent()
+      .contains('小室哲哉')
     cy.get(':nth-child(1) > :nth-child(6) > :nth-child(2) > a')
-    .should('not.be')
+      .should('not.be')
     cy.go('back')
   });
 
@@ -91,7 +82,7 @@ describe('template spec', () => {
 
     cy.intercept('GET', 'https://musicbrainz.org/ws/2/artist/?query=artist:the%20band%20apart&offset=0&limit=100&fmt=json', { fixture: 'mock_the_band_apart.json' }).as('theBandApartRequest');
     cy.get('#search').focus().clear()
-    .type('the band apart{enter}', {force: true});
+      .type('the band apart{enter}', {force: true});
     cy.contains('検索').first().click();
     cy.wait('@theBandApartRequest');
     cy.contains('the band apart')
