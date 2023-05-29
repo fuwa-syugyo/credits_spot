12cdad0b3243279aa4bcabf438ca26a0fa20d8aa
fuwa <fuwafuwa41.glr@gmail.com>
2023-05-29 22:34:37 +0900

    RecordingSearchをキャメルケースに

--- src/components/recordings/RecordingSearch.vue
+++ src/components/recordings/RecordingSearch.vue
@@ -10,34 +10,34 @@ import NowLoading from '../NowLoading.vue'
 import NoResults from '../NoResults.vue'
 
 onBeforeRouteUpdate((to, from, next) => {
-  recording_term.value = (to.query.term as string) || ''
+  recordingTerm.value = (to.query.term as string) || ''
   currentPage.value = 1
-  onClickHandler(currentPage.value, recording_term.value).then(() => {
+  onClickHandler(currentPage.value, recordingTerm.value).then(() => {
     next()
   })
 })
 
 const route = useRoute()
-const recording_term = ref((route.query.term as string) || '')
-const recording_data = ref<SearchRecordingData[]>([])
+const recordingTerm = ref((route.query.term as string) || '')
+const recordingData = ref<SearchRecordingData[]>([])
 
 const selectFilter = ref<Array<string>>([])
 const artistName = ref()
 const isLoading = ref(false)
 
-const onClickHandler = async (page: number, recording_term: string) => {
-  if (!recording_term) {
-    recording_term = (route.query.term as string) || ''
+const onClickHandler = async (page: number, recordingTerm: string) => {
+  if (!recordingTerm) {
+    recordingTerm = (route.query.term as string) || ''
   }
 
   const offset = (page - 1) * 100
   try {
     isLoading.value = true
     const data = await fetch(
-      `https://musicbrainz.org/ws/2/recording/?query=recording:${recording_term}&offset=${offset}&limit=100&fmt=json`
+      `https://musicbrainz.org/ws/2/recording/?query=recording:${recordingTerm}&offset=${offset}&limit=100&fmt=json`
     ).then((res) => res.json())
 
-    const new_recording_data: SearchRecordingData[] = data.recordings
+    const newRecordingData: SearchRecordingData[] = data.recordings
       .filter((rec: SearchRecordingData) => rec)
       .map((item: SearchRecordingData) => ({
         id: item.id,
@@ -45,15 +45,15 @@ const onClickHandler = async (page: number, recording_term: string) => {
         'artist-credit': item['artist-credit'].map((credit) => ({
           id: credit.artist.id,
           name: credit.artist.name,
-          join_phrase: credit.joinphrase,
-          all_name:
+          joinPhrase: credit.joinphrase,
+          allName:
             credit.artist.name +
             (credit.joinphrase ? ' ' + credit.joinphrase : ''),
         })),
         first_release_date: item['first-release-date'],
       }))
 
-    recording_data.value = new_recording_data
+    recordingData.value = newRecordingData
     totalItems.value = data.count
   } catch {
     console.error('Error fetching data:', Error)
@@ -78,7 +78,7 @@ const applyFilter = (): void => {
   router.push({
     name: 'RecordingSearchFilter',
     query: {
-      term: recording_term.value,
+      term: recordingTerm.value,
       getRidOfInstrument: getRidOfInstrumentValue,
       getPartialMatch: getPartialMatchValue,
       artistName: artistName.value,
@@ -87,64 +87,53 @@ const applyFilter = (): void => {
 }
 
 onMounted(() => {
-  onClickHandler(currentPage.value, recording_term.value)
+  onClickHandler(currentPage.value, recordingTerm.value)
 })
 </script>
 
 <template>
   <div v-if="isLoading">
-    <NowLoading />
+    <NowLoading></NowLoading>
   </div>
-  <div
-    v-else-if="recording_data.length !== 0"
-    class="container"
-  >
-    <h1 class="text-2xl my-4 max-w-xl">
-      楽曲検索結果
-    </h1>
+  <div v-else-if="recordingData.length !== 0" class="container">
+    <h1 class="text-2xl my-4 max-w-xl">楽曲検索結果</h1>
     <div
       class="px-4 my-4 border border-gray-500 py-4 md:w-[350px] w-[250px] rounded-md"
     >
-      <form @submit.prevent="applyFilter">
-        <p class="text-xl mb-2">
-          絞り込み
-        </p>
+      <form v-on:submit.prevent="applyFilter">
+        <p class="text-xl mb-2">絞り込み</p>
         <div class="bg-slate-100 flex-col mb-2">
-          <label
-            for="inst"
-            class="mr-[10px] flex"
-          ><input
-             id="inst"
-             v-model="selectFilter"
-             type="checkbox"
-             value="getRidOfInstrument"
-           >
+          <label for="inst" class="mr-[10px] flex"
+            ><input
+              type="checkbox"
+              v-model="selectFilter"
+              value="getRidOfInstrument"
+              id="inst"
+            />
             <span>インスト音源以外</span>
           </label>
-          <label
-            for="partial"
-            class="flex"
-          ><input
-             id="partial"
-             v-model="selectFilter"
-             type="checkbox"
-             value="getPartialMatch"
-           >
+          <label for="partial" class="flex"
+            ><input
+              type="checkbox"
+              v-model="selectFilter"
+              value="getPartialMatch"
+              id="partial"
+            />
             <span>部分一致の曲</span>
           </label>
         </div>
         <label>アーティスト名</label>
         <div class="relative">
           <input
-            id="filter"
             v-model="artistName"
             type="search"
+            id="filter"
             class="p-2 pl-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-1"
             placeholder="アーティスト名を入力"
-          >
+          />
           <div
             class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-          />
+          ></div>
           <button
             type="submit"
             :disabled="!selectFilter[0] && !artistName"
@@ -158,23 +147,19 @@ onMounted(() => {
     <p>
       {{
         '検索結果 ' +
-          totalItems +
-          ' 件中 ' +
-          ((currentPage - 1) * 100 + 1) +
-          ' 〜 ' +
-          ((currentPage - 1) * 100 + recording_data.length) +
-          '件'
+        totalItems +
+        ' 件中 ' +
+        ((currentPage - 1) * 100 + 1) +
+        ' 〜 ' +
+        ((currentPage - 1) * 100 + recordingData.length) +
+        '件'
       }}
     </p>
     <table class="table-auto my-4">
       <thead>
         <tr>
-          <th class="px-4 py-2 border w-[400px] bg-blue-100">
-            曲名
-          </th>
-          <th class="px-4 py-2 border w-[400px] bg-blue-100">
-            アーティスト
-          </th>
+          <th class="px-4 py-2 border w-[400px] bg-blue-100">曲名</th>
+          <th class="px-4 py-2 border w-[400px] bg-blue-100">アーティスト</th>
           <th
             class="px-4 py-2 border w-[130px] bg-blue-100 hidden md:inline-block"
           >
@@ -184,13 +169,13 @@ onMounted(() => {
       </thead>
       <tbody>
         <tr
-          v-for="recording in recording_data"
+          v-for="recording in recordingData"
           :key="recording.id"
           class="border px-4 py-2"
         >
           <td class="border px-4 py-2 max-w-[600px]">
             <RouterLink
-              :to="{
+              v-bind:to="{
                 name: 'RecordingDetail',
                 params: { id: recording.id },
               }"
@@ -201,7 +186,7 @@ onMounted(() => {
           <td class="border px-4 py-2">
             {{
               recording['artist-credit']
-                .map((credit: ArtistCredit) => credit.all_name)
+                .map((credit: ArtistCredit) => credit.allName)
                 .join(' ')
             }}
           </td>
@@ -213,16 +198,16 @@ onMounted(() => {
     </table>
     <div v-if="totalItems > 100">
       <vue-awesome-paginate
-        v-model="currentPage"
         :total-items="totalItems"
         :items-per-page="100"
         :max-pages-shown="3"
+        v-model="currentPage"
         :on-click="onClickHandler"
       />
     </div>
   </div>
   <div v-else>
-    <NoResults />
+    <NoResults></NoResults>
   </div>
 </template>
 
