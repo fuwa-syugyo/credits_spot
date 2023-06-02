03b7a041085b95400c5d766e98cebe76333464c4
fuwa <fuwafuwa41.glr@gmail.com>
2023-06-02 14:25:00 +0900

    入力フォームのダークモード設定を削除

--- src/components/recordings/RecordingSearch.vue
+++ src/components/recordings/RecordingSearch.vue
@@ -126,7 +126,7 @@ onMounted(() => {
             id="filter"
             v-model="artistName"
             type="search"
-            class="p-2 pl-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-1"
+            class="p-2 pl-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 my-1"
             placeholder="アーティスト名を入力"
           />
           <div
