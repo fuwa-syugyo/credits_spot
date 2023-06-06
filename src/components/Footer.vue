fe7ae85e0458fed05595ff71833e19fbfc5046ae
fuwa <fuwafuwa41.glr@gmail.com>
2023-06-06 21:51:13 +0900

    フッターの表示順を変更

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="my-8 container md:w-[930px]">
-    <div class="md:flex my-2 mx-2">
-      <div class="mr-auto text-slate-600 py-2">© 2023 fuwa-syugyo</div>
-      <div class="px-2 py-2">
+    <div class="md:flex my-2">
+      <div class="py-2">
         <RouterLink :to="{ name: 'Caution' }"> ご利用上の注意 </RouterLink>
       </div>
       <div class="px-2 py-2">
@@ -11,6 +10,7 @@
       <div class="px-2 py-2">
         <a href="https://twitter.com/fuwa_syugyo">Twitter</a>
       </div>
+      <div class="ml-auto text-slate-600 py-2">© 2023 fuwa-syugyo</div>
     </div>
   </div>
 </template>
