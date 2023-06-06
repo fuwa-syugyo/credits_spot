56b53bbcf279bf28a7d6738735d6603efc7b6b7b
fuwa <fuwafuwa41.glr@gmail.com>
2023-06-06 21:47:39 +0900

    スマホ時のフッターの表示それぞれに余白

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="my-8 container md:w-[930px]">
     <div class="md:flex my-2 mx-2">
-      <div class="mr-auto text-slate-600">© 2023 fuwa-syugyo</div>
-      <div class="px-2">
+      <div class="mr-auto text-slate-600 py-2">© 2023 fuwa-syugyo</div>
+      <div class="px-2 py-2">
         <RouterLink :to="{ name: 'Caution' }"> ご利用上の注意 </RouterLink>
       </div>
-      <div class="px-2">
+      <div class="px-2 py-2">
         <a href="https://github.com/fuwa-syugyo/credits_spot">GitHub</a>
       </div>
-      <div class="px-2">
+      <div class="px-2 py-2">
         <a href="https://twitter.com/fuwa_syugyo">Twitter</a>
       </div>
     </div>
