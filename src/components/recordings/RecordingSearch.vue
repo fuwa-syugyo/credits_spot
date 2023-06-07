0d7864e500717d3ff55dbb245034afb4cbba63d4
fuwa <fuwafuwa41.glr@gmail.com>
2023-06-08 00:05:08 +0900

    空白微調整

--- src/components/recordings/RecordingSearch.vue
+++ src/components/recordings/RecordingSearch.vue
@@ -96,7 +96,7 @@ onMounted(() => {
   <div v-else-if="refRecordingData.length !== 0" class="container">
     <h1 class="text-2xl my-4 max-w-xl">音源検索結果</h1>
     <p>音源をクリックすると、その音源に関わったスタッフを確認できます。</p>
-    <div class="my-4 border md:w-[450px] w-[250px] rounded-md">
+    <div class="my-2 border md:w-[450px] w-[250px] rounded-md">
       <form @submit.prevent="applyFilter">
         <div class="border-b-2 px-4 bg-blue-200">
           <p class="text-lg mb-2 font-bold pt-2">絞り込み</p>
