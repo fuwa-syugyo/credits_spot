c23a5f42eb9fe52c34a302c5d30bb7a0718190b4
fuwa <fuwafuwa41.glr@gmail.com>
2023-07-05 17:32:39 +0900

    fetch失敗&通常時の曲名テスト追加

--- cypress/component/RecordingInWork.cy.ts
+++ cypress/component/RecordingInWork.cy.ts
@@ -13,9 +13,28 @@ describe('More than 100 recordings', () => {
     } as OptionsParam)
     cy.wait('@debbyRequest')
     cy.get('h1').contains('曲群一覧')
+    cy.contains('Waltz for Debby')
 
     cy.get('.work-table > tbody > tr').should(($trs) => {
       expect($trs, '127 items').to.have.length(127)
     })
+  })
+})
+
+describe('More than 100 recordings', () => {
+  it('False fetch request', () => {
+    cy.intercept(
+      'GET',
+      'https://musicbrainz.org/ws/2/work/30a33711-0db8-3fc7-a3c8-f42426bdf43b?inc=recording-rels+artist-credits&fmt=json',
+      { forceNetworkError: true,
+        fixture: 'mock_debby.json' }
+    ).as('debbyRequest')
+    cy.mount(RecordingInWork, {
+      props: { id: '30a33711-0db8-3fc7-a3c8-f42426bdf43b' },
+    } as OptionsParam)
+    cy.wait('@debbyRequest')
+
+    cy.contains('時間を置いて再度お試しください。')
+    cy.contains('Waltz for Debby').should('not.be')
   })
 })
